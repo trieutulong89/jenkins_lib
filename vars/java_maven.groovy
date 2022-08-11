6b537cb4ba9b7d1864eea3161a0565fc4687cf95
thanhvan <thanhvanptit@outlook.com>
2022-08-11 22:12:30 +0700

    update Jenkinsfile

--- vars/java_maven.groovy
+++ vars/java_maven.groovy
@@ -1,6 +1,7 @@
 def call() {
   env.PATH = "${tool 'maven'}/bin:${env.PATH}"
   packageApp()
+  buildImage()
 }
 
 // ===================================
@@ -11,5 +12,18 @@ def packageApp() {
   print("Doing package app")
   stage('Package') {
     sh "mvn package -Dmaven.test.skip"
+  }
+}
+
+// ===================================
+// Build docker image
+// Description: Build docker image and push to docker hub
+// ===================================
+
+def buildImage() {
+  print 'Doing build docker image'
+  image_tag = "java-test:1"
+  stage('Build Docker Image') {
+    image = docker.build("${image_tag}")
   }
 }
